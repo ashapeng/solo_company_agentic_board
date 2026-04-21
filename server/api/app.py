f6316f16f9de3281db1364fb37ebc2909e708bcc
Peng <apengnaruto@gmail.com>
2026-04-20 21:02:33 -0400

    feat(api): restrict CORS methods and headers to explicit allow-list

--- server/api/app.py
+++ server/api/app.py
@@ -30,8 +30,8 @@ app.add_middleware(
         "http://127.0.0.1:8000",
         "http://localhost:8000",
     ],
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "OPTIONS"],
+    allow_headers=["content-type", "authorization"],
 )
 
 
